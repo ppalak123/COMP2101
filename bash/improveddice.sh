9c91823ba0ddd761c0085e207fd1312f1baaab58
ppalak <200455454@student.georgianc.on.ca>
2021-10-14 14:00:34 +0300

    Lab 2 improceddice.sh practice

---
+++ bash/improveddice.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# this script rolls a pair of six-sided dice and displays both the rolls
+#
+
+# Task 1:
+#  put the number of sides in a variable which is used as the range for the random number
+#  put the bias, or minimum value for the generated number in another variable
+#  roll the dice using the variables for the range and bias i.e. RANDOM % range + bias
+
+# Task 2:
+#  generate the sum of the dice
+#  generate the average of the dice
+
+#  display a summary of what was rolled, and what the results of your arithmetic were
+
+# Tell the user we have started processing
+echo "Rolling..."
+
+sides=6
+bias=1
+# roll the dice and save the results
+die1=$(( RANDOM % sides + bias))
+die2=$(( RANDOM % sides + bias ))
+
+sum=$(( die1 + die2 ))
+avg=$(( sum / 2 ))
+
+# display the results
+echo "Rolled $die1, $die2"
+echo "Sum: $sum, average: $avg"
