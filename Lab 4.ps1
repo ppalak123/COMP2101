b5e6cf3bcf5d2024e7bc20599aaee2da77327966
Palak <palaksingla451@gmail.com>
2021-12-15 19:24:27 -0800

    Create Lab 4.ps1

---
+++ Lab 4.ps1
@@ -0,0 +1,60 @@
+﻿#Function for getting system hardware description
+echo "System Hardware Description"
+$systemhardware = Get-WmiObject win32_computersystem
+$systemhardware | Select-Object | ft description
+
+#Function for getting operating system data
+echo "Operating System Data"
+$osdata = Get-WmiObject win32_operatingsystem
+$osdata | Select-Object | ft name, version
+
+#Function for getting processor information
+echo "Processor Information:"
+$processordata = Get-WmiObject win32_processor
+$processordata | Select-Object | ft Description, MaxClockSpeed, NumberOfCores, L3CacheSize
+
+#function for getting RAM description
+echo "RAM Description"
+$capacity = 0
+Get-WmiObject -class win32_physicalmemory | foreach {
+New-Object -TypeName psobject -Property @{
+Manufacturer = $_.manufacturer
+Description = $_.Description
+"Size(GB)" = $_.capacity/1gb
+Bank = $_.Banklabel
+Slot = $_.devicelocator }
+$capacity += $_.capacity/1mb } | ft Vendor, Description, "Size(MB)", Bank, Slot
+"Total RAM: ${capacity}MB"
+""
+
+#Function for getting the information of physical disk drives
+echo "Physical Disk Drives Information"
+get-WmiObject -classname Win32_DiskDrive |
+where-object DeviceID -ne $NULL |
+Foreach-Object {
+                $drive = $_
+                $drive.GetRelated("Win32_DiskPartition") |
+                foreach {$logicaldisk =$_.GetRelated("win32_LogicalDisk");
+                        if($logicaldisk.size) {
+                                New-Object -TypeName PSobject -Property @{
+    Manufacturer = $drive.Manufacturer
+    DriveLetter = $logicaldisk.DeviceID
+    Model = $drive.model
+    Size = [String]($logicaldisk.size/1gb -as [int])+"GB"
+    Free = [string]((($logicaldisk.freespace / $logicaldisk.size) * 100) -as [int])+ "%"
+    FreeSpace = [string]($logicaldisk.freespace / 1gb -as [int]) +"GB"
+      } | ft -AutoSize Manufacturer, Model, size, Free, FreeSpace }
+   }
+  }
+
+
+#Function for getting network adaptor configuration
+echo "Network Adapter Configuration"
+$adaptersinfo = get-ciminstance win32_networkadapterconfiguration 
+$adaptersinfo | Where-Object ipenabled -eq 1 | 
+Select-Object | ft Description, Index, IPAddress, IPSubnet, DNSDomain, @{n="DNSServer";e={$_.DNSServerSearchOrder }}
+
+# Function for getting video card summary
+echo "Video Card Summary"
+$gpu = Get-WmiObject win32_videocontroller
+$gpu | Select-Object | ft @{n="Vendor";e={$.AdapterCompatibility}}, Description, @{n="Screen Resolution";e={$.VideoModeDescription}}
