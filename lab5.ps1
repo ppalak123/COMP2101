366b6d6158f7e0518539da44a5dc50acf280bbbf
Palak <palaksingla451@gmail.com>
2021-12-16 18:06:32 -0800

    Create lab5.ps1

---
+++ lab5.ps1
@@ -0,0 +1,27 @@
+﻿param (
+    [switch]$System,
+    [switch]$Disks,
+    [switch]$Network
+)
+if ( !($System) -and !($Disks) -and !($Network)) {
+    hardwareInfo
+    processorData
+    osData
+    ramData
+    graphicInfo
+    diskInfo
+    networkInfo
+}
+if ($System) {
+    hardwareInfo
+    processorData
+    OSData
+    ramData
+    graphicsInfo
+}
+if ($Disks) {
+    diskInfo
+}
+if ($Network) {
+    networkInfo
+}
